f5a0f1348ec8a945d5bac660f8475b0831f9da0f
xinwang <tobilon@outlook.com>
2020-05-24 17:00:24 +0800

    refuse call with fund less than 0.1 eth

--- contracts/Startup.sol
+++ contracts/Startup.sol
@@ -2,6 +2,9 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract Startup
 {
+    address private _owner;
+    address payable private _coinbase;
+
     struct Profile {
         string name;
         string category;
@@ -11,9 +14,28 @@ contract Startup
 
     mapping (string => Profile) startups;
 
+    modifier isOwner() {
+        require(msg.sender == _owner);
+        _;
+    }
+
+    constructor()
+        public {
+        _owner = msg.sender;
+    }
+
+    function setCoinBase(address addr)
+        isOwner
+        public {
+        _coinbase = addr;
+    }
+
     function newStartup(string memory id, string memory name, string memory category, string memory misson, string memory desc) public payable {
+        require(msg.value >= 1e17);
+        require(_coinbase != address(0));
         Profile memory p = Profile(name, category, misson, desc);
         startups[id] = p;
+        _coinbase.transfer(msg.value);
     }
 
     function getStartup(string memory id)
