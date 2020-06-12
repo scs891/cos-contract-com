392c114dd871b090f25f8bf2072ff057d7a077d4
xinwang <tobilon@outlook.com>
2020-06-12 13:47:08 +0800

    add IRO

--- contracts/IRO.sol
+++ contracts/IRO.sol
@@ -9,16 +9,14 @@ contract IRO
         _;
     }
 
-    struct WalletAddr{
-        string name;
-        address addr;
-    }
-
-    struct IRO {
+    struct TokenSetting {
         string tokenName;
         string tokenSymbol;
         string tokenAddr;
-        WalletAddr[] walletAddrs;
+        address[] walletAddrs;
+    }
+
+    struct VoteSetting {
         string voteType;
         string voteTokenLimit;
         address[] voteAssignAddrs;
@@ -27,11 +25,51 @@ contract IRO
         string voteMinDurationHours;
         string voteMaxDurationHours;
     }
-    
-    mapping (string => IRO) IROs;
+
+    struct Setting {
+        TokenSetting tokenSetting;
+        VoteSetting voteSetting;
+    }
+
+    mapping (string => Setting) IROs;
 
     constructor()
         public {
         _owner = msg.sender;
+    }
+
+    function newSetting(string memory id,
+                        string memory tokenName, string memory tokenSymbol, string memory tokenAddr,
+                        address[] memory walletAddrs,
+                        string memory voteType, string memory voteTokenLimit, address[] memory voteAssignAddrs,
+                        string memory voteMSupportPercent, string memory voteMinApprovalPercent,
+                        string memory voteMinDurationHours, string memory voteMaxDurationHours)
+                        public
+                        payable {
+        TokenSetting memory tokenSetting = TokenSetting(tokenName, tokenSymbol, tokenAddr, walletAddrs);
+        VoteSetting memory voteSetting = VoteSetting(voteType, voteTokenLimit,
+                                                     voteAssignAddrs,voteMSupportPercent,voteMinApprovalPercent,
+                                                     voteMinDurationHours, voteMaxDurationHours);
+        IROs[id] = Setting(tokenSetting, voteSetting);
+    }
+
+    function getTokenSetting(string memory id)
+        public
+        view
+        returns (string memory tokenName, string memory tokenSymbol, string memory tokenAddr,
+                address[] memory walletAddrs){
+        return (IROs[id].tokenSetting.tokenName, IROs[id].tokenSetting.tokenSymbol,
+                IROs[id].tokenSetting.tokenAddr, IROs[id].tokenSetting.walletAddrs);
+    }
+
+    function getVoteSetting(string memory id)
+        public
+        view
+        returns (string memory voteType, string memory voteTokenLimit, address[] memory voteAssignAddrs,
+                 string memory voteMSupportPercent, string memory voteMinApprovalPercent,
+                 string memory voteMinDurationHours, string memory voteMaxDurationHours){
+        return (IROs[id].voteSetting.voteType, IROs[id].voteSetting.voteTokenLimit, IROs[id].voteSetting.voteAssignAddrs,
+                IROs[id].voteSetting.voteMSupportPercent, IROs[id].voteSetting.voteMinApprovalPercent,
+                IROs[id].voteSetting.voteMinDurationHours, IROs[id].voteSetting.voteMaxDurationHours);
     }
 }
--- migrations/2_deploy_startup.js
+++ migrations/2_deploy_startup.js
@@ -1,5 +1,7 @@
 const Startup = artifacts.require("Startup");
+const IRO = artifacts.require("IRO");
 
 module.exports = function(deployer) {
   deployer.deploy(Startup);
+  deployer.deploy(IRO);
 };
