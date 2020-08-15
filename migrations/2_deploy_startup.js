394d62e8b160657f96da2087ec7a9d0829a2fd08
superestze <sbura@sina.com>
2020-08-15 23:35:46 +0800

    把增加转账的合约部署

--- migrations/2_deploy_startup.js
+++ migrations/2_deploy_startup.js
@@ -1,9 +1,11 @@
 const Startup = artifacts.require('Startup')
 const IRO = artifacts.require('IRO')
 const Bounty = artifacts.require('Bounty')
+const Follow = artifacts.require('follow')
 
 module.exports = function (deployer) {
 	deployer.deploy(Startup)
 	deployer.deploy(IRO)
 	deployer.deploy(Bounty)
+	deployer.deploy(Follow)
 }
