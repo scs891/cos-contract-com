632f77999d3c1957f9264fe176c1e18ec731beb5
superestze <sbura@sina.com>
2020-12-15 00:36:49 +0800

    feat✨ : 增加合约事件

--- contracts/Disco.sol
+++ contracts/Disco.sol
@@ -4,8 +4,10 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract Disco {
   addrsss private _owner;
+  unit public time;
 
-    struct Disco {
+  // disco
+  struct Disco {
     address walletAddr;
     address tokenContract;
     string description;
@@ -19,8 +21,49 @@ contract Disco {
     uint256 totalDepositToken;
   }
 
-  mapping (string => Disco) discos;
+  // disco 投资人信息
+  struct DiscoInvestor {
+    // 投资人地址
+    address investor;
+    // 投资金额, 可以多次投资
+    uint[] value;
+     // 时间
+    string[] time;
+    // 募资比例获得TOKEN
+    uint256 sharedToken;
+    // 奖励TOKEN
+    uint256 rewardedToken;
+  }
 
+  // disco的状态
+  struct DiscoStatus {
+    // 募资是否结束
+    bool isFinished;
+    // 募资是否成功
+    bool isSuccess;
+    // 募资是否开启；
+    bool isEnabled;
+  }
+
+  // 记录创建的disco
+  mapping (string => Disco) public discos;
+  // 记录投资人
+  mapping (string => DiscoInvestor) public investors;
+  // 记录投资的状态
+  mapping (string => DiscoStatus) public status;
+
+  // 创建
+  event createdDisco(discoId, disco);
+  // 开启
+  event enabeldDisco(discoId, disco);
+  // 募资成功的时候
+  event fundraisingSuccessed(discoId, disco);
+  //募资结束（成功）
+  event fundraisingFinished(discoId, disco);
+  // 募资失败
+  event fundraisingFailed(discoId, disco);
+
+  // 判断是否是本人
   modifier isOwner() {
     require(msg.sender == _owner);
     _;
@@ -34,7 +77,28 @@ contract Disco {
   function setCoinBase(address payable addr) isOwner public {
     _coinbase = addr;
   }
-  
+
+  function getCoinBase() returns(address) {
+    return _coinbase;
+  }
+
+  function getDate() public returns(unit){
+    time = now;
+    return time;
+  }
+
+   /**
+     * 募资转账 TODO
+    */
+  function () payable {
+      // require('募资没有结束');
+      uint amount = msg.value;
+      balanceOf[msg.sender] += amount;
+      // amountRaised += amount;
+      // tokenReward.transfer(msg.sender, amount / price);
+      // TODO
+      FundTransfer(msg.sender, amount, true);
+  }
 
   // 创建Disco
   function newDisco(
@@ -51,6 +115,7 @@ contract Disco {
     uint256 addLiquidityPool;
     uint256 totalDepositToken;
   ) {
+    require(!this.discos[id]);
     Disco memory d = Disco(
       walletAddr,
       tokenContract,
@@ -63,39 +128,69 @@ contract Disco {
       minFundRaising,
       addLiquidityPool,
       totalDepositToken,
+      false,
+      false
     );
+    DiscoStatus memory s = DiscoStatus(
+      false,
+      false,
+      false,
+    )
     discos[id] = d;
+    status[id] = s;
+    
+    // disco 创建成功
+    emit createdDisco(id, discos[id])
+  }
+
+    // 开启募资
+  function enableDisco(string memory id) public {
+    reuqire(disco[id] &&  discos[id].fundRaisingTimeFrom > getDate() && discos[id].fundRaisingTimeTo < getData());
+    require(!status[id].isEnabled);
+    status[id].isEnabled = true;
+    // 发送开启募资的事件
+    emit enabeldDisco(id, discos[id]);
   }
 
 
   // 获取 disco
-  function getDisco(string memory id) public view returns(
-    address walletAddr,
-    address tokenContract,
-    string description,
-    string fundRaisingTimeFrom,
-    string fundRaisingTimeTo,
-    uint256 investmentReward,
-    uint256 rewardDeclineRate,
-    uint256 shareToken,
-    uint256 minFundRaising,
-    uint256 addLiquidityPool,
-    uint256 totalDepositToken,
-  ) {
+  function getDisco(string memory id) public view returns(Disco) {
     return (
-      discos[id].walletAddr,
-      discos[id].walletAddr
-      discos[id].tokenContract
-      discos[id].description
-      discos[id].fundRaisingTimeFrom
-      discos[id].fundRaisingTimeTo
-      discos[id].investmentReward
-      discos[id].rewardDeclineRate
-      discos[id].shareToken
-      discos[id].minFundRaising
-      discos[id].addLiquidityPool
-      discos[id].totalDepositToken
+      discos[id];
     );
   }
 
+  // 募资结束
+  function finishedDisco(string memory id) public returns(bool) {
+    require(status[id].isFinished !== true);
+    status[id].isFinished = true;
+
+    // TODO 结束的时候需要检查募资是否成功或者失败
+    if ('成功') {
+      status[id].isSuccess = true
+    } else {
+      statue[id].isSuccess = false;
+    }
+    emit fundraisingFinished(id);
+  }
+
+  // 发起募资, 记录募资的信息， 可能会多次募资
+  function investor(
+      string memory id,
+      address address,
+      uint256 value,
+      string time,
+      uint256 sharedToken,
+      uint256 rewardedToken,
+
+    ) public {
+     DiscoInvestor memory i = DiscoInvestor(
+      address, 
+      [value], 
+      [time]
+      sharedToken,
+      rewardedToken
+    )
+    investors[id] = i;
+  }
 }
