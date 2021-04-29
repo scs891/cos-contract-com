cec1e318234c8f3203617978d6e881956a1badd3
yinshen <yinshen@huli.com>
2021-04-29 23:41:15 +0800

    vote

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -7,6 +7,7 @@ import "./Disco.sol";
 import "./common/Base.sol";
 import "./common/FundPool.sol";
 import "./interfaces/IErc20.sol";
+import {NumUtils} from "./libraries/NumUtils.sol";
 
 contract Proposal is Base
 {
@@ -43,6 +44,7 @@ contract Proposal is Base
         Payment payment;
         ProposerSetup proposerSetup;
         VoterSetup voteSetup;
+        uint256 blockTime;
     }
 
     struct Payment {
@@ -68,18 +70,30 @@ contract Proposal is Base
     struct VoterSetup {
         string voteMSupportPercent;
         string voteMinApprovalPercent;
-        string voteMinDurationHours;
-        string voteMaxDurationHours;
+        uint256 voteDurationHours;
+        uint256 voteEndTime;
+    }
+
+    struct Vote {
+        string discoId;
+        string serialId;
+        address voter;
+        uint256 pos;
+        uint256 neg;
+        uint256 voteBt;
     }
 
     mapping(string => mapping(string => ProposalDetail)) private discoProposals;
     //unsupported copy struct memory xxx[] to xxx, so create mapping to save the payment details.
-    mapping(string => PaymentDetail[]) proposalPaymentDetails;
+    mapping(string => PaymentDetail[]) private proposalPaymentDetails;
     mapping(string => uint256) private countDiscoProposals;
+    mapping(string => Vote[]) private votes;
 
     event accepted(ProposalDetail indexed proposal, PaymentDetail[] indexed paymentDetails);
 
     event changeProposalStatus(string indexed id, ProposalStatus indexed status);
+
+    event voteAction(Vote indexed v);
 
     function accept(ProposalDetail memory proposal, PaymentDetail[] memory paymentDetails) public payable returns (ProposalDetail memory) {
         require(bytes(proposal.serialId).length != 0, "proposal serialId is empty!");
@@ -87,7 +101,6 @@ contract Proposal is Base
         mapping(string => ProposalDetail) storage discoProposalMapper = discoProposals[proposal.discoId];
         ProposalDetail memory checkProposal = discoProposalMapper[proposal.serialId];
         require(bytes(checkProposal.discoId).length == 0, "proposal already exists, please replace with the contract owner");
-
 
         IRO.Setting memory baseSetting = _iroBase.setting(proposal.discoId);
         require(bytes(baseSetting.id).length != 0, "base setting not exists.");
@@ -98,21 +111,34 @@ contract Proposal is Base
 
         IRO.VoterSetting memory voterSetting = baseSetting.voterSetting;
         require(bytes(voterSetting.voteType).length != 0, "voter setting not exists.");
-        proposal.voteSetup = VoterSetup(voterSetting.voteMSupportPercent, voterSetting.voteMinApprovalPercent, voterSetting.voteMinDurationHours, voterSetting.voteMaxDurationHours);
+
+        uint256 bt = block.timestamp;
+        uint256 voteDurationHours;
+        if (proposal.voteSetup.voteDurationHours == 0) {
+            //string to uint
+            voteDurationHours = NumUtils.stringToUint(voterSetting.voteMinDurationHours);
+        } else {
+            voteDurationHours = proposal.voteSetup.voteDurationHours;
+        }
+
+        proposal.voteSetup = VoterSetup(voterSetting.voteMSupportPercent, voterSetting.voteMinApprovalPercent, voteDurationHours, bt + voteDurationHours * 3600);
 
         //lock init proposal token into a pool.
         IERC20 token = _discoBase.discoToken(proposal.discoId);
         proposal.payment.token = token;
         //cal poolId
-        string memory poolId = string(abi.encodePacked(proposal.discoId, "@", proposal.serialId));
+        string memory poolId = getPoolId(proposal);
         proposal.payment.pool = new FundPool(poolId);
-        token.transferFrom(msg.sender, proposal.payment.pool.getAddress(), proposal.payment.totalAmount);
+        if (proposal.payment.totalAmount > 0) {
+            token.transferFrom(msg.sender, proposal.payment.pool.getAddress(), proposal.payment.totalAmount);
+        }
 
         uint256 paymentDetailsSize = paymentDetails.length;
         for (uint256 i = 0; i < paymentDetailsSize; i++) {
             proposalPaymentDetails[poolId].push(paymentDetails[i]);
         }
-
+        //record block once
+        proposal.blockTime = bt;
         discoProposalMapper[proposal.serialId] = proposal;
         countDiscoProposals[proposal.discoId]++;
         emit accepted(proposal, paymentDetails);
@@ -121,7 +147,7 @@ contract Proposal is Base
 
     function proposalDetail(string calldata id, string calldata serialId) external view returns (ProposalDetail memory, PaymentDetail[] memory paymentDetails){
         ProposalDetail memory proposal = internalProposal(id, serialId);
-        string memory poolId = string(abi.encodePacked(proposal.discoId, "@", proposal.serialId));
+        string memory poolId = getPoolId(proposal);
         return (proposal, proposalPaymentDetails[poolId]);
     }
 
@@ -142,7 +168,7 @@ contract Proposal is Base
     /**
     * decide proposal status.
     **/
-    function decide(string calldata id, string calldata serialId, ProposalStatus status) external{
+    function decide(string calldata id, string calldata serialId, ProposalStatus status) external {
         ProposalDetail memory proposal = internalProposal(id, serialId);
         require(bytes(proposal.discoId).length != 0, "proposal missing.");
         if (status != proposal.status) {
@@ -160,7 +186,7 @@ contract Proposal is Base
     **/
     function proposalStatus(string calldata id, string calldata serialId, ProposalStatus status) external returns (ProposalStatus){
         ProposalDetail memory proposal = internalProposal(id, serialId);
-        require(bytes(proposal.discoId).length != 0, "proposal missing.");
+        require(bytes(proposal.discoId).length != 0, "proposal missing, check first.");
         return proposal.status;
     }
 
@@ -172,21 +198,49 @@ contract Proposal is Base
         return proposal;
     }
 
+    /**
+    * vote when proposal is voting.
+    * lock into the fundPool.
+    **/
+    function vote(Vote memory v) public payable {
+        ProposalDetail memory proposal = internalProposal(v.discoId, v.serialId);
+        require(bytes(v.discoId).length != 0, "proposal missing, check first.");
+        require(proposal.status == ProposalStatus.Voting, "the proposal could not be voted when status is not voting.");
+        VoterSetup memory setup = proposal.voteSetup;
+        uint256 bt = block.timestamp;
+        require(bt <= setup.voteEndTime, "vote is expired.");
+        require(v.pos + v.neg > 0, "a invalid vote, vote num <= 0.");
+        IERC20 token = _discoBase.discoToken(proposal.discoId);
+        token.transferFrom(msg.sender, proposal.payment.pool.getAddress(), v.pos + v.neg);
+        v.voteBt = block.timestamp;
+        v.voter = msg.sender;
+        votes[v.discoId].push(v);
+        emit voteAction(v);
+    }
+
     //owner manage.
     function fullSet(ProposalDetail memory proposal, PaymentDetail[] memory paymentDetails) public isOwner returns (ProposalDetail memory) {
         require(bytes(proposal.serialId).length != 0, "proposal serialId is empty!");
         require(bytes(proposal.discoId).length != 0, "proposal discoId is empty!");
         mapping(string => ProposalDetail) storage discoProposalMapper = discoProposals[proposal.discoId];
         discoProposalMapper[proposal.serialId] = proposal;
-        string memory poolId = string(abi.encodePacked(proposal.discoId, "@", proposal.serialId));
+        string memory poolId = getPoolId(proposal);
         uint256 paymentDetailsSize = paymentDetails.length;
         for (uint256 i = 0; i < paymentDetailsSize; i++) {
             proposalPaymentDetails[poolId].push(paymentDetails[i]);
         }
-        emit accepted(proposal,paymentDetails);
+        emit accepted(proposal, paymentDetails);
         return proposal;
     }
 
+    //internal utils
+    function getPoolId(string memory discoId, string memory serialId) internal pure returns (string memory){
+        return string(abi.encodePacked(discoId, "@", serialId));
+    }
+
+    function getPoolId(ProposalDetail memory proposal) internal pure returns (string memory){
+        return getPoolId(proposal.discoId, proposal.serialId);
+    }
 
     function setIROBase(address _iroAddress) public isOwner {
         require(_iroAddress != address(0), "iro address is empty.");
---
+++ contracts/libraries/NumUtils.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.6;
+
+library NumUtils {
+    function stringToUint(string memory s) public pure returns (uint) {
+        bytes memory b = bytes(s);
+        uint result = 0;
+        for (uint i = 0; i < b.length; i++) {
+            if (uint8(b[i]) >= 48 && uint8(b[i]) <= 57) {
+                result = result * 10 + (uint8(b[i]) - 48);
+            }
+        }
+        return result;
+    }
+}
